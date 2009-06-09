d2be2e5da9411dae31b2e10e268cf3aa9e9e3639
eduardo <e.leoni@gmail.com>
2009-06-09 17:04:51 -0400

    Procedure to download some or all bio data from camara.

---
+++ R/biodownload.R
@@ -0,0 +1,7 @@
+##to download all
+##system(paste("wget -nd -E -Nr -P ../data/bio/all 'http://www.camara.gov.br/internet/deputado/DepNovos_Lista.asp?fMode=1&forma=lista&SX=QQ&Legislatura=QQ&nome=&Partido=QQ&ordem=nome&condic=QQ&UF=QQ&Todos=sim'",sep=''))
+
+##download sessions
+for (session in sessions) {
+  system(paste("wget -nd -E -Nr -P ../data/bio/",session," 'http://www.camara.gov.br/internet/deputado/DepNovos_Lista.asp?fMode=1&forma=lista&SX=QQ&Legislatura=",session,"&nome=&Partido=QQ&ordem=nome&condic=QQ&UF=QQ&Todos=sim'",sep=''))
+}
