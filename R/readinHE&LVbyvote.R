3d282ab79107a1da16538830c0e18f1f7cfc32eb
eduardo <e.leoni@gmail.com>
2009-06-09 22:03:38 -0400

    mods by Eduardo: fixed a couple of bugs; fixed file encoding; we have to see what to do with tha paths (runfrom) since we working from two different computers; used uzip instead of zip.unpack (which i dont have)

--- R/readinHE&LVbyvote.R
+++ R/readinHE&LVbyvote.R
@@ -1,7 +1,15 @@
-#SHould work for current downloads
+## TODO: What should we do about the last day of the month? Perhaps download the current month and the last?
+
+##SHould work for current downloads
 #Older downloads, prior to 2009, have subdirectories in the zip files, use four digit years, etc...
-run.from<-"C:/DATA/NECON/"
-source(paste(run.from,"/DATA/Functions/_cleanname.fnct.R",sep=""))  
+##run.from<-"C:/DATA/NECON/"
+##source(paste(run.from,"/DATA/Functions/_cleanname.fnct.R",sep=""))  
+##source(paste(run.from,"~/R/_cleanname.fnct.R",sep=""))
+run.from<-"~/reps/CongressoAberto/data/NECON/"
+source("~/reps/CongressoAberto/R/_cleanname.fnct.R")
+current.encoding <- getOption("encoding")
+## set as iso
+options(encoding="ISO8859-1")
 
 
 #Get current date, and move to appropriate directory
@@ -10,46 +18,57 @@ current.year <- format(Sys.time(), "%Y")
 setwd(paste(run.from,current.year,sep=""))  
 SFfiles <- grep("SF",dir(),value=TRUE)
 if(length(SFfiles)>0){file.remove(SFfiles)} #Get rid of SENADO files, if they exist
-
-
 ### See current files, download this months`s data, flag new votes
 old.LVfiles <- grep("LV",dir(),value=TRUE)  #get already coded votes
-                                            #we could change this so that it uses the csv votes (already tabulated) as the reference
- 
-        #Download current months's zip
-        monthspt<-c("Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro")
-        month <- monthspt[current.month]
-        year <- substr(current.year,3,4)
-        the.url <- paste("http://www.camara.gov.br/internet/plenario/result/votacao/",month,year,".zip",sep="")
-        try(download.file(the.url,dest=paste(month,current.year,".zip",sep=""),quiet = FALSE, mode="wb"),silent=TRUE) #continue even if some file doesn't exist
-        #Unzip this months's zip file
-         zip.unpack(paste(month,current.year,".zip",sep=""), dest=getwd())
-         SFfiles <- grep("SF",dir(),value=TRUE)
-         if(length(SFfiles)>0){file.remove(SFfiles)} #Get rid of SENADO files, if they were downloaded
+##we could change this so that it uses the csv votes (already tabulated) as the reference
+##Download current months's zip
+monthspt<-c("Janeiro","Fevereiro","Marco","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro")
+month <- monthspt[current.month]
+year <- substr(current.year,3,4)
+the.url <- paste("http://www.camara.gov.br/internet/plenario/result/votacao/",month,year,".zip",sep="")
+try(download.file(the.url,dest=paste(month,current.year,".zip",sep=""),quiet = FALSE, mode="wb"),silent=TRUE) #continue even if some file doesn't exist
+##Unzip this months's zip file
+##zip.unpack(paste(month,current.year,".zip",sep=""), dest=getwd())
+## the following code extracts to the current dir junking paths (i.e.)
+##    it does not create directories, putting everything in the same place
+unzip(paste(month,current.year,".zip",sep=""),junkpaths=TRUE)
+
+SFfiles <- grep("SF",dir(),value=TRUE)
+if(length(SFfiles)>0){file.remove(SFfiles)} #Get rid of SENADO files, if they were downloaded
 
 new.LVfiles<-grep("LV",dir(),value=TRUE) 
 votes <- setdiff(new.LVfiles,old.LVfiles) #compare new files with old to flag recently downloaded
 
-for(i in 1:length(votes)){  #for each new vote, create two new files
-    LVfile <- votes[i]  
-    #Read data from VOTE LIST file for the vote
-    if(nchar(vote)==24){ #formato antigo: titulo tinha 24 characters, no novo so 21
-            LV <- read.fwf(LVfile, widths=c(9,-1,9,40,10,10,25,4),strip.white=TRUE) }else{
-            LV <- read.fwf(LVfile, widths=c(9,-1,6,40,10,10,25,4),strip.white=TRUE) }
+nvotes <- length(votes)
+if (nvotes>0) {
+  file.table <- matrix("",nvotes,2)  
+  for(i in 1:nvotes) {  #for each new vote, create two new files
+    file.table[i,1] <- LVfile <- votes[i]  
+    ##Read data from VOTE LIST file for the vote
+    ##if(nchar(vote)==24){ #formato antigo: titulo tinha 24 characters, no novo so 21
+    ##Fixed the following line (I think)
+    if(nchar(LVfile)==24)  { #formato antigo: titulo tinha 24 characters, no novo so 21
+      LV <- read.fwf(LVfile, widths=c(9,-1,9,40,10,10,25,4),strip.white=TRUE)
+    }  else {
+      LV <- read.fwf(LVfile, widths=c(9,-1,6,40,10,10,25,4),strip.white=TRUE)
+    }
     voteid <- LV$V2[1]  #store number of vote for future use
     names(LV) <- c("sessionid","voteid","name",paste("vote",voteid,sep="."),"party","state","id") #rename fields
-    rc$name<-clean.name(LV) #apply cleaning function for accents and other characters
+    LV$name<-clean.name(LV) #apply cleaning function for accents and other characters
     levels(LV$state)<-c("AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG",       #change state names to acronyms
-                         "PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO") #Think whether this is kosher
+                        "PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO") #Think whether this is kosher
     LV <- LV[,c("id","name","party","state",paste("vote",voteid,sep="."))] #rearrange fields
     write.csv(LV,file=paste(gsub("txt","csv",LVfile)),row.names = FALSE) #save file 
-    #Read data from HEADER file for the vote
+    ##Read data from HEADER file for the vote
     headerfiles<-grep("HE",dir(),value=TRUE) #get summary files with dates
-    HEfile<-grep(as.character(voteid),headerfiles,value=TRUE) #find appropriate HE file, and read line by line...
+    file.table[i,2] <- HEfile<-grep(paste(as.character(voteid),".*txt$",sep=''),headerfiles,value=TRUE) #find appropriate HE file, and read line by line...    
     vt.date<-as.Date(as.character(read.table(HEfile, header = FALSE, nrows = 1,skip = 2, strip.white = TRUE, as.is = TRUE)[1,1]), "%d/%m/%Y")
     vt.descrip<-read.table(HEfile, header = FALSE, nrows = 1,skip = 12, strip.white = TRUE, as.is = TRUE, sep=";",quote="")
     vt.session<-read.table(HEfile, header = FALSE, nrows = 1,skip = 0, strip.white = TRUE, as.is = TRUE)[1,1]
     vt.descrip<-gsub("\"","",vt.descrip)    #get rid of quotes in the description of the bill
     HE <- data.frame(voteid,date=vt.date,session=vt.session,description=vt.descrip)
     write.csv(HE,file=paste(gsub("txt","csv",HEfile)),row.names = FALSE)
+  }
+  file.table <- gsub("txt","csv",file.table)
+  ## do something (load db, post, graphs, whatever)
 }
