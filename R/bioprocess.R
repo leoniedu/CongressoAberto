aa773b6588573c057520066c03c69c7923ebc5d8
eduardo <e.leoni@gmail.com>
2009-06-10 21:55:49 -0400

    Reorganized and added the needed manual fixes in the end. I think we are able to find everyone now both in bio and in roll calls.

--- R/bioprocess.R
+++ R/bioprocess.R
@@ -1,13 +1,18 @@
+
+
 ##process bio files
 ## creates: bio.all (one row per legislator)
 ##          idname (one row per legislator/name/session
                                         # names written in multiple forms)
 
+##FIX: Get the party/state from the page index. 
+
+
 library(plyr)
 library(RMySQL)
 source("~/reps/CongressoAberto/R/caFunctions.R")
 
-
+connect.db()
 
 gb <- function(x) trim(toupper(gsub(".*<b>(.*)<.*b>.*","\\1",x)))
 
@@ -62,12 +67,36 @@ get.bio <- function(file.now) {
   gc()
   data.frame(nameshort, name=namelong, partynow=party, state=state, birth=birthdate, birthplace, sessions=sessions, parties=parties , mandates,bioid=id,biofile=file.now,imagefile)
 }
+
+##to download all (uncomment this to download the file)
+##system(paste("wget -nd -E -Nr -P ../data/bio/all 'http://www.camara.gov.br/internet/deputado/DepNovos_Lista.asp?fMode=1&forma=lista&SX=QQ&Legislatura=QQ&nome=&Partido=QQ&ordem=nome&condic=QQ&UF=QQ&Todos=sim'",sep=''))
+
+index.file <- "../data/bio/all/DepNovos_Lista.asp?fMode=1&forma=lista&SX=QQ&Legislatura=QQ&nome=&Partido=QQ&ordem=nome&condic=QQ&UF=QQ&Todos=sim.html"
+ll <- readLines(index.file,encoding='latin1')
+ll <- gsub("\t+| +"," ",ll)
+##pe <- ll[grep("[A-Z] - [A-Z]",ll)]
+peloc <- grep("/[A-Z]{2}<",ll)
+pe <- trim(ll[peloc])
+pe <- gsub("</b>","",pe)
+pe <- strsplit(pe,"/")
+np <- sapply(pe,function(x) x[[1]])
+uf <- sapply(pe,function(x) x[[length(x)]])
+np <- strsplit(np,"-")
+name <- sapply(np,function(x) trim(x[[1]]))
+partido.current <- sapply(np,function(x) trim(x[[2]]))
+id <- gsub(".*id=([0-9]+)&.*","\\1",ll[peloc-1])
+
+##manual fix: there is a mistake in the camara website of the
+## code of "tatico"
+## we recode it here
+## id[id=='108697'] <- '520630'
+## system(paste("wget -nd -E -Nr -P ../data/bio/all 'http://www.camara.gov.br/internet/deputado/DepNovos_Detalhe.asp?id=108697&leg=QQ'",sep=''))
+
+data.legis <- data.frame(bioid=id,nameindex=name,state=uf)##,partido.current=partido.current)
+
 files.list <- dir('../data/bio/all/',pattern="DepNovos_Detalhe",full.names=TRUE)
 bio.all <- lapply(files.list,get.bio)
 bio.all <- do.call(rbind,bio.all)
-
-##manual fixes
-bio.all[bio.all$bioid=="96883","state"] <- "AP"
 
 ## create a deputyid/name/session to use when merging data from
 ## multiple sources (this is slow and memory consuming)
@@ -75,13 +104,16 @@ idname <- with(bio.all,
                data.frame(bioid,
                           name,                         
                           nameshort,
-                          state,
+                          ##state,
                           sessions))
+idname <- merge(idname,data.legis)
+bio.all <- merge(subset(bio.all,select=-state),data.legis)##,by="bioid")
 idname <- ddply(idname,'bioid',
                 function(x) 
                 with(x,data.frame(bioid,
                                   name,
                                   nameshort,
+                                  nameindex,
                                   state,
                                   sessions=strsplit(as.character(sessions),",")[[1]]
                                   )
@@ -89,26 +121,72 @@ idname <- ddply(idname,'bioid',
                 .progress="text")
 idname <- with(idname,rbind(
                             data.frame(bioid,name=as.character(name),state,sessions),
-                            data.frame(bioid,name=as.character(nameshort),state,sessions))
+                            data.frame(bioid,name=as.character(nameshort),state,sessions),
+                            data.frame(bioid,name=as.character(nameindex),state,sessions))
                )
 idname <- unique(idname)
+idname$id <- ""
 
 
-
-
-##try to join this to votos db
-## votos.id <- dbGetQuery(connect, "select distinct br_votos.id, br_votos.name, br_votos.state, br_votacoes.anolegislativo from br_votos,br_votacoes where br_votos.origvote=br_votacoes.origvote")
 
 connect.db()
 
 
-dbRemoveTable(connect,"br_idname")
+dbRemoveTable(connect,"br_bioidname")
 dbRemoveTable(connect,"br_bio")
 
-dbWriteTable(connect, "br_idname", idname, overwrite=TRUE,
+dbWriteTable(connect, "br_bioidname", idname, overwrite=TRUE,
              row.names = F, eol = "\r\n" )    
 
 dbWriteTable(connect, "br_bio", bio.all, overwrite=TRUE,
              row.names = F, eol = "\r\n" )    
 
 
+connect.db()
+
+##manual fixes
+source("~/reps/CongressoAberto/R/caFunctions.R")
+connect.db()
+
+
+## PHILEMON RODRIGUES was a deputy both in MG and in PB
+dbSendQuery(connect,"update br_bioidname set state='PB' where (bioid='98291')")
+dbSendQuery(connect,"update br_bioidname set state='MG' where (bioid='98291') AND (sessions!='2003-2007')")
+dbGetQuery(connect,"select * from  br_bioidname where bioid='98291'")
+
+
+##tatico deputi in both DF and GO
+dbSendQuery(connect,"update br_bioidname set state='DF' where (bioid='108697') AND (sessions='2003-2007')")
+dbSendQuery(connect,"update br_bioidname set state='GO' where (bioid='108697') AND (sessions='2007-2011')")
+dbGetQuery(connect,"select * from  br_bioidname where bioid='108697'")
+
+## ze indio: 100486
+tmp <- iconv.df(dbGetQuery(connect,"select * from  br_bioidname where bioid='100486'"))
+tmp$name <- 'JOSÉ ÍNDIO'
+tmp <- unique(tmp)
+dbWriteTable(connect, "br_bioidname", tmp, overwrite=FALSE,append=TRUE,
+             row.names = F, eol = "\r\n" )
+dedup.db('br_bioidname')
+
+
+## Mainha is José de Andrade Maia Filho 182632
+tmp <- iconv.df(dbGetQuery(connect,"select * from  br_bioidname where bioid='182632'"))
+tmp$name <- 'MAINHA'
+tmp <- unique(tmp)
+dbWriteTable(connect, "br_bioidname", tmp, overwrite=FALSE,append=TRUE,
+             row.names = F, eol = "\r\n" )
+dedup.db('br_bioidname')
+
+
+
+
+
+
+##Pastor Jorge is Jorge dos Reis Pinheiro 100606
+tmp <- iconv.df(dbGetQuery(connect,"select * from  br_bioidname where bioid='100606'"))
+
+tmp$name <- 'PASTOR JORGE'
+tmp <- unique(tmp)
+dbWriteTable(connect, "br_bioidname", tmp, overwrite=FALSE,append=TRUE,
+             row.names = F, eol = "\r\n" )
+dedup.db('br_bioidname')
