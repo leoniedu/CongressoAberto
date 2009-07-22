64c533b5271cc340a6c8de5af4cdd8a5a9d99689
eduardo <e.leoni@gmail.com>
2009-07-22 00:33:17 -0400

    daily cron job

---
+++ R/daily.R
@@ -0,0 +1,19 @@
+## daily cron job for R
+
+download.now <- TRUE
+update.all <- FALSE
+rf <- function(x=NULL) {
+  if (.Platform$OS.type!="unix") {
+    run.from <- "C:/reps/CongressoAberto"
+  } else {
+    run.from <- "~/reps/CongressoAberto"
+  }
+  ## side effect: load functions
+  source(paste(run.from,"/R/caFunctions.R",sep=""))
+  if (is.null(x)) {
+    run.from
+  } else {
+    paste(run.from,"/",x,sep='')
+  }
+}
+source(rf("R/updateVot.R",echo=TRUE))
