7e47068d5cf148ff0aea10bc178d16fa65c87fa6
eduardo <e.leoni@gmail.com>
2009-08-08 08:04:40 -0400

    first commit

---
+++ R/daily.sh
@@ -0,0 +1,3 @@
+source /home/leoniedu/.bash_profile
+rm /home/leoniedu/reps/CongressoAberto/data/camara/rollcalls/*.zip;rm /home/leoniedu/reps/CongressoAberto/data/camara/rollcalls/extracted/*
+/home/leoniedu/run/bin/R --vanilla < ~/reps/CongressoAberto/R/daily.R
