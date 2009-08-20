eadf82eb4a8d01afd8483ac31f66cb86554bcc8b
eduardo <e.leoni@gmail.com>
2009-08-20 15:43:35 -0400

    save

---
+++ R/presenca.R
@@ -0,0 +1,140 @@
+## Model the most absent legislators
+## Basic idea: random effects model with legislator+roll call+party random effects.
+## to be Updated monthly?
+
+## paths (put on the beg of R scripts)
+rf <- function(x=NULL) {
+  if (.Platform$OS.type!="unix") {
+      run.from <- "C:/reps/CongressoAberto"
+    } else {
+      run.from <- "~/reps/CongressoAberto"
+    }
+  ## side effect: load functions
+  source(paste(run.from,"/R/caFunctions.R",sep=""),encoding="utf8")
+  if (is.null(x)) {
+    run.from
+  } else {
+    paste(run.from,"/",x,sep='')
+  }
+}
+rf()
+
+connect.db()
+
+
+sql <- paste("select  a.*, b.*, cast(b.rcdate as date) as rcdate  from br_votacoes as b, br_votos as a  where a.rcfile=b.rcfile  and  (rcdate>cast('",Sys.Date()-365,"' as date)) ",sep='')
+res <- dbGetQueryU(connect,sql)
+
+res$presente <- as.numeric(res$rc!="Ausente")
+
+
+library(lme4)
+
+## model1. bioid
+m1 <- glmer(presente~(1|bioid),data=res,family=binomial)
+
+## model2. party,bioid
+m2<- glmer(presente~(1|party)+(1|bioid),data=res,family=binomial)
+
+## model3. party,bioid,rc
+m3<- glmer(presente~(1|party)+(1|bioid)+(1|rcfile),data=res,family=binomial)
+
+
+m3z<- zelig(presente~tag(1|party)+tag(1|bioid)+tag(1|rcfile),data=res,model="logit.mixed")
+
+## model3. party,bioid,rc
+## state does not help predict abstentions
+## date does not help either (just a linear term might help a little bit)
+## billtype neither
+##m4<- glmer(presente~(1|party)+(1|bioid)+(1|rcfile)+(1|state),data=res,family=binomial)
+
+x.high <- setx(m3z, bioid="119138")
+x.low <-  setx(m3z, bioid=160448)
+
+
+res$date <- as.numeric(as.Date(res$rcdate))
+res$date <- res$date-min(res$date)
+res$date2 <- res$date^2
+
+m5<- glmer(presente~(1|party)+(1|bioid)+(1|rcfile)+billtype,data=res,family=binomial)
+## date might help
+
+
+## get current list of legislators
+deps <- dbReadTableU(connect,"br_deputados_current")
+
+## check if all parties are present
+if (!all(unique(deps$party)%in%rownames(ranef(m3z)$party))) stop("parties missing")
+## party ranef as df
+rparty <- data.frame(ranef(m3z)$party)
+rparty$party <- rownames(rparty)
+names(rparty)[1] <- c("party.ranef")
+
+## bioid ranef as df
+rbio <- data.frame(ranef(m3z)$bioid)
+rbio$bioid <- rownames(rbio)
+names(rbio)[1] <- c("bioid.ranef")
+
+## generate predicted values
+pred <- merge(deps,rparty)
+pred <- merge(pred,rbio)
+if (nrow(pred)!=(nrow(deps))) stop("deps not matched")
+pred$pres.score <- with(pred,party.ranef+bioid.ranef)
+pred$pres.score <- round(plogis(pred$pres.score)*100)/100
+
+library(ggplot2)
+
+
+
+
+phist <- function(data,i,oneplot=FALSE) {
+  p1 <- ggplot(data,aes(pres.score))
+  p1 <- p1+geom_histogram(aes(y = ..count..), binwidth = 0.05,fill="gray30")
+  p1 <- p1+scale_x_continuous(name="",limits=c(0,1),expand=c(0,0),formatter="percent")+scale_y_continuous(name="")+
+    theme_bw()
+  if (oneplot) {
+    ds <- subset(pred,party==pred$party[i])
+    p1 <- p1+geom_histogram(data=ds,fill="blue")
+  }
+  p1 <- p1+geom_vline(xintercept=pred$pres.score[i],col="red",size=2)
+  p1
+}
+
+i <- sample(1:nrow(pred),1)
+##i <- 373
+rel <- prop.table(table(pred$abs.score<pred$abs.score[i]))["TRUE"]
+if (is.na(rel)) rel <- 0
+p1 <- phist(pred,i)
+p1 <- p1+geom_text(data=data.frame(x=.75,y=max(table(cut(p1$data$pres.score,seq(0,1,.05))))*.9,label="",pres.score=.5),mapping=aes(x=x,y=y,label=label),size=20,colour="darkblue")
+p1
+
+ds <- subset(pred,party==pred$party[i])
+p2 <- phist(ds,i)
+p2 <- p2+geom_text(data=data.frame(x=.75,y=max(table(cut(p2$data$pres.score,seq(0,1,.05))))*.9,label=pred$party[i],pres.score=.5),mapping=aes(x=x,y=y,label=label),size=20,colour="darkblue")+
+  theme_bw()
+
+Layout <- grid.layout( nrow = 2, ncol = 1
+                      ,widths = unit(c(2,2), c("null","null") ),
+                      heights = unit (c(1,1), c("null", "null") )
+                      )
+vplayout <- function (...) {
+  grid.newpage()
+  pushViewport(viewport(
+                        layout= Layout
+                        ))
+}
+subplot <- function(x, y) viewport(layout.pos.row=x, layout.pos.col=y)
+vplayout()
+print(p1, vp=subplot(1,1))
+print(p2, vp=subplot(2,1))
+
+
+
+
+
+## Do simulations to get the percentile (and conf interv) of the legislators in
+## terms of abstenteism
+
+
+
+
