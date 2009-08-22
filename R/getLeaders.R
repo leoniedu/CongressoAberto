5fca2e89681b302cc1e967d2706dd111e8b1a7bc
eduardo <e.leoni@gmail.com>
2009-08-22 09:55:24 -0400

    small changes to getleaders

---
+++ R/getLeaders.R
@@ -0,0 +1,25 @@
+## paths (put on the beg of R scripts)
+rf <- function(x=NULL) {
+  if (.Platform$OS.type!="unix") {
+      run.from <- "C:/reps/CongressoAberto"
+    } else {
+      run.from <- "~/reps/CongressoAberto"
+    }
+  ## side effect: load functions
+  source(paste(run.from,"/R/caFunctions.R",sep=""),encoding="utf8")
+  if (is.null(x)) {
+    run.from
+  } else {
+    paste(run.from,"/",x,sep='')
+  }
+}
+rf()
+
+connect.db()
+
+
+vot <- dbGetQueryU(connect,"select * from br_votacoes")
+
+##vots <- ddply(vot,"rcyear",function(x) x[sample(1:nrow(x),3,replace=TRUE),])
+##vots <- subset(vot,rcyear=="1998")
+res <- lapply(vot$rcvoteid,getLeaders)
