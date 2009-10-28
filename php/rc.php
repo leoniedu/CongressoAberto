5511b6c61898ce13793fe1c14d428c6f1daa797f
Eduardo Leoni <eduardo@neguinha.local>
2009-10-28 01:00:45 -0400

    trying out a table

--- php/rc.php
+++ php/rc.php
@@ -1,3 +1,36 @@
+<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+<script type="text/javascript">
+  google.load('visualization', '1', {packages: ['table']});
+</script>
+<script language="javascript" type="text/javascript">
+  var rcvoteid = "<?php echo $rcvoteid; ?>";
+</script>
+<script type="text/javascript">
+var visualization;
+var data;
+var options = {'showRowNumber': true};
+function drawVisualization() {
+  var query = new google.visualization.Query('/php/query.php?form=rcvotes1&rcvoteid='+rcvoteid);    
+    query.send(handleQueryResponse);
+}
+function handleQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+  var data = response.getDataTable();
+  options['page'] = 'enable';
+  options['pageSize'] = 20;
+  options['pagingButtonsConfiguration'] = 'auto';
+  options['allowHtml'] = true;
+  var view = new google.visualization.DataView(data);
+  visualization = new google.visualization.Table(document.getElementById('table1'));
+  visualization.draw(view, options);
+}
+ google.setOnLoadCallback(drawVisualization);
+ 
+ </script>
+
 <script language="php">
 include_once("server.php");
 
@@ -17,22 +50,46 @@ echo '<p>Proposição (clique para ver detalhes): <a href="'.get_permalink($row[
 
 echo '<p>Ementa: "'.$row[3].'"</p>' ;
 
-
-
-
 the_excerpt();
 
-echo '<h3> Resultado da Votação </h3>';
-echo '<a href="/php/timthumb.php?src=/images/rollcalls/bar'.$rcvoteid.'large.png">   <img src="/php/timthumb.php?src=/images/rollcalls/bar'.$rcvoteid.'large.png&h=300&w=300&zc=0&q=100"> </a>' ;
+//echo '<h3> Resultado da Votação </h3>';
+//echo '<a href="/php/timthumb.php?src=/images/rollcalls/bar'.$rcvoteid.'large.png">   <img src="/php/timthumb.php?src=/images/rollcalls/bar'.$rcvoteid.'large.png&h=300&w=300&zc=0&q=100"> </a>' ;
 // echo '<img src="/images/rollcalls/bar'.$rcvoteid.'large.png"  width=400 >'  ;
 
-echo '<h3> Votação por partido </h3>';
-echo '<img src="/php/timthumb.php?src=/images/rollcalls/mosaic'.$rcvoteid.'large.png&w=500&zc=0&q=100">' ;
+//echo '<h3> Votação por partido </h3>';
+//echo '<img src="/php/timthumb.php?src=/images/rollcalls/mosaic'.$rcvoteid.'large.png&w=500&zc=0&q=100">' ;
 
 
-echo '<h3> Votação por estado </h3>';
-echo '<img src="/php/timthumb.php?src=/images/rollcalls/map'.$rcvoteid.'large.png&w=350&zc=0&q=100">' ;
+//echo '<h3> Votação por estado </h3>';
+//echo '<img src="/php/timthumb.php?src=/images/rollcalls/map'.$rcvoteid.'large.png&w=350&zc=0&q=100">' ;
 
-print("<p><a href=\"/php/query.php?form=rcvotes&rcvoteid=$rcvoteid&tqx=reqId:0;out:csv;csvFile:votacao_rc$rcvoteid\"> Download da votação em formato csv</a></p>");
+
+echo '<table>
+<tr>
+<td><img src="/php/timthumb.php?src=/images/rollcalls/map'.$rcvoteid.'large.png&w=300&zc=0&q=100">
+</td>
+</tr>
+<tr>
+<td>
+<img src="/php/timthumb.php?src=/images/rollcalls/mosaic'.$rcvoteid.'large.png&w=400&zc=0&q=100">
+</td>
+</tr>
+<tr>
+<td>
+<img src="/php/timthumb.php?src=/images/rollcalls/bar'.$rcvoteid.'large.png&w=300&zc=0&q=100">
+</td>
+</tr>
+</table>';
+
 
 </script>
+
+<p><h3> Tabela com o Resultado da Votação </h3></p>
+
+<div id="table1">
+</div>
+
+
+<?php 
+print("<p><a href=\"/php/query.php?form=rcvotes&rcvoteid=$rcvoteid&tqx=reqId:0;out:csv;csvFile:votacao_rc$rcvoteid\"> Download da votação em formato csv</a></p>");
+?>
